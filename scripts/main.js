46abd64c3400d28869cdb19c7e3bd68d7c735fe1
Thomas Edgeworth <tom.edgeworth47@gmail.com>
2019-05-13 11:24:17 -0400

    Chuck facts are updated via 5 second intervals

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,23 @@
 "use strict"
 
-const getMoreQuotesButton = document.getElementById('getMoreQuotes');
-getMoreQuotesButton.addEventListener('click', function(e){
-    e.preventDefault();
-    updateChuckSays();
-});
+// const getMoreQuotesButton = document.getElementById('getMoreQuotes');
+// getMoreQuotesButton.addEventListener('click', function(e){
+//     e.preventDefault();
+//     updateChuckSays();
+// });
 
 function updateChuckSays() {
-    const chuckSays = document.getElementById('chuckSays');
-    
     get('https://api.chucknorris.io/jokes/random?category=dev')
     .then((response) => {
-        chuckSays.innerHTML = response;
+        // This used to update paragraph but we're not doing that anymore
+        // chuckSays.innerHTML = response;
+        addItem(response);
     });
+}
+
+function addItem(item) {
+    const chuckQuotes = document.getElementById('chuckQuotes');
+    const theQuoteItem = document.createElement('li');
+    theQuoteItem.textContent = item;
+    chuckQuotes.append(theQuoteItem);
 }
